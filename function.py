2e07c94335a4be7c8c3977bc4b8cc5d525dad83d
simran_grewal <simrangrewal587@gmail.com>
2020-08-03 22:49:17 -0700

    removed id and password

--- function.py
+++ function.py
@@ -1,10 +1,14 @@
 from scrape import InstagramBot
 
+user_name = ""
+password = ""
+
+
 def get_images(user, name, tags):
-    bot = InstagramBot('7783250627', 'Langara!')
+    bot = InstagramBot(user_name, password)
 
     profiles = name.split(' ')
     hashTags = tags.split(' ')
-    
+
     images = bot.search(profiles, hashTags)
     return images
